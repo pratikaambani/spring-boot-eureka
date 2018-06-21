0002cff62dfb413e027eef5add4534bdf6591125
Pratik Ambani <pratikaambani@gmail.com>
2018-06-21 12:23:38 -0800

    added spring cloud eureka server #8761

---
+++ README.md
@@ -0,0 +1,18 @@
+###Project title
+These services are part of Spring Cloud Practise.
+
+###Screens
+
+###Libraries used
+Spring Boot 1.4.1.RELEASE
+spring-cloud-starter-eureka-server
+
+###Features
+These services can,
+ - Boot up Eureka Server
+
+###Installing
+Run EurekaServerApplication
+
+###Developers
+Pratik Ambani(pratikaambani@gmail.com)
---
+++ eureka-server/src/main/java/com/practise/eureka/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.practise.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Pratik Ambani on 21/6/18.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
